b350be651244762188711d5885ef0bbc70fced28
Karan Bihani <84910173+karanbihani@users.noreply.github.com>
2023-10-01 19:34:19 +0530

    Add files via upload

--- storage.go
+++ storage.go
@@ -21,42 +21,42 @@ import (
 // 	return os.Getenv(key)
 // }
 
-type Storage interface{
+type Storage interface {
 	CreateAccount(*Account) error
 	UpdateAccount(*Account) error
 	DeleteAccount(int) error
 	GetAccounts() ([]*Account, error)
-	GetAccountByID(int)(*Account, error)
+	GetAccountByID(int) (*Account, error)
 }
 
-type PostgresStore struct{
+type PostgresStore struct {
 	db *sql.DB
 }
 
-func NewPostgresStore()(*PostgresStore, error){
+func NewPostgresStore() (*PostgresStore, error) {
 	connStr := "user=postgres dbname=postgres password=bihani123 sslmode=disable"
-    
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println("Database connection is successful!")
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Database connection is successful!")
 	return &PostgresStore{
-		db:db,
-	},nil
+		db: db,
+	}, nil
 }
 
-func (s *PostgresStore) Init() error{
+func (s *PostgresStore) Init() error {
 	return s.CreateAccountTable()
 }
 
-func (s *PostgresStore) CreateAccountTable() error{
+func (s *PostgresStore) CreateAccountTable() error {
 	query := `create table if not exists account(
 		id serial primary key,
 		first_name varchar(50),
@@ -65,51 +65,69 @@ func (s *PostgresStore) CreateAccountTable() error{
 		balance serial,
 		created_at timestamp
 	)`
-	_,err := s.db.Exec(query)
+	_, err := s.db.Exec(query)
 	return err
 }
 
-func (s *PostgresStore) CreateAccount(acc *Account) error{
-	query:= "insert into account (first_name,last_name,number,balance,created_at) values ($1,$2,$3,$4,$5)"
-	
-	resp,err := s.db.Query(query, acc.FirstName, acc.LastName, acc.Number, acc.Balance, acc.CreatedAt)
+func (s *PostgresStore) CreateAccount(acc *Account) error {
+	query := "insert into account (first_name,last_name,number,balance,created_at) values ($1,$2,$3,$4,$5)"
 
-	if err != nil{
+	resp, err := s.db.Query(query, acc.FirstName, acc.LastName, acc.Number, acc.Balance, acc.CreatedAt)
+
+	if err != nil {
 		return err
 	}
 
 	fmt.Printf("%+v\n", resp)
-	
+
 	return nil
 }
 
-func (s *PostgresStore) UpdateAccount(*Account) error{
+func (s *PostgresStore) UpdateAccount(*Account) error {
 	return nil
 }
 
-func (s *PostgresStore) DeleteAccount(id int) error{
+func (s *PostgresStore) DeleteAccount(id int) error {
 	return nil
 }
 
-func (s *PostgresStore) GetAccountByID(id int) (*Account, error){
-	return nil,nil
-}
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
 
-func (s *PostgresStore) GetAccounts() ([]*Account, error){
-	rows,err := s.db.Query("select * from account")
+	row, err := s.db.Query("select * from account where id = $1", id)
 
 	if err !=nil{
-		return nil,err
+		return nil, err
+	}
+
+	for row.Next(){
+		return ScanIntoAccount(row)
+	}
+
+	return nil, fmt.Errorf("account %d not found", id)
+}
+
+func (s *PostgresStore) GetAccounts() ([]*Account, error) {
+	rows, err := s.db.Query("select * from account")
+
+	if err != nil {
+		return nil, err
 	}
 
 	accounts := []*Account{}
 
-	for rows.Next(){
-		account := new(Account)
-		if err := rows.Scan(&account.ID, &account.FirstName, &account.LastName, &account.Number, &account.Balance, &account.CreatedAt); err!=nil{
+	for rows.Next() {
+
+		account, err := scanIntoAccount(rows)
+		if err != nil {
 			return nil, err
 		}
 		accounts = append(accounts, account)
 	}
 	return accounts, nil
+}
+
+func ScanIntoAccount(rows *sql.Rows) (*Account, error) {
+	account := new(Account)
+	if err := rows.Scan(&account.ID, &account.FirstName, &account.LastName, &account.Number, &account.Balance, &account.CreatedAt)
+	return account, error
 }
---
+++ user_api.go
@@ -0,0 +1,125 @@
+package main
+
+//Lessons
+/*
+1. don't camelCase when in differnt files it doesn't have "scope" or something to access
+2. package name SHOULD be main (why need to check)
+3. use go run . and not specific file else everything doesn't compile and throws error
+*/
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type APIFunc func(http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+func MakeHTTPHandleFunc(f APIFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAdder string
+	store       Storage
+}
+
+func NewAPIServer(listenAdder string, store Storage) *APIServer {
+	return &APIServer{
+		listenAdder: listenAdder,
+		store:       store,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", MakeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", MakeHTTPHandleFunc(s.handleGetAccountById))
+
+	log.Println("JSON API server running on port : ", s.listenAdder)
+
+	http.ListenAndServe(s.listenAdder, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	// if r.Method == "UPDATE"{
+	// 	return s.handleGetAccount(w, r)
+	// }
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	accounts, err := s.store.GetAccounts()
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, accounts)
+}
+
+func (s *APIServer) handleGetAccountById(w http.ResponseWriter, r *http.Request) error {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil{
+		return fmt.Errorf("invalid id given %s", idStr)
+	}
+
+	account, err := s.store.GetAccountByID(id)
+
+	if err !=nil{
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	createAccountReq := new(CreateAccountRequest)
+	//createAccountReq := CreateAccountRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&createAccountReq); err != nil {
+		return err
+	}
+
+	account := NewAccount(createAccountReq.FirstName, createAccountReq.LastName)
+	if err := s.store.CreateAccount(account); err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
